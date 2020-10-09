e38186963901d3547869cce06d6fa938d35d522e
Jason Axelson <jason.axelson@gmail.com>
2020-10-08 22:48:55 -1000

    Various improvements

--- assets/js/force_layout.js
+++ assets/js/force_layout.js
@@ -1,3 +1,7 @@
+import { CustomTooltip } from './utils/custom_tooltip.js'
+
+const tooltip = CustomTooltip("node_tooltip", 300)
+
 export function forceLayout(dataPromise) {
   dataPromise.then(data => {
     render(data)
@@ -8,13 +12,31 @@ function render(data) {
   const nodeData = data.filter(row => row.type == "node")
   const linkData = data.filter(row => row.type == "edge")
 
-  const width = 500, height = 500
+  const width = 1000, height = 1000
 
   d3.forceSimulation(nodeData)
-    .force('charge', d3.forceManyBody())
+    .force('charge', d3.forceManyBody().strength(chargeStrength))
     .force('center', d3.forceCenter(width / 2, height / 2))
     .force('link', d3.forceLink().links(linkData).id(item => item.id))
     .on('tick', buildTicked(nodeData, linkData));
+}
+
+function chargeStrength(_data) {
+  // NOTE: It might be nice for this to be dependent on the number of connected
+  // edges. By giving more strength to nodes that have many edges the clusters
+  // will be a little more dispersed and hopefully easier to grok.
+  return -50
+}
+
+function showTooltip(data) {
+  let content = "<div class=\"inner_tooltip\">"
+  content += `<span class=\"file-name\">${data.id}</span><br/>`
+  content += "</div>"
+  tooltip.showTooltip(content, d3.event)
+}
+
+function hideTooltip() {
+  tooltip.hideTooltip()
 }
 
 function buildTicked(nodeData, linkData) {
@@ -32,6 +54,13 @@ function updateLinks(linkData) {
   u.enter()
     .append('line')
     .merge(u)
+    .attr('stroke', function (d) {
+      if (d.label == "(compile)") {
+        return 'red'
+      } else {
+        return '#ccc'
+      }
+    })
     .attr('x1', function(d) {
       return d.source.x
     })
@@ -56,6 +85,7 @@ function updateNodes(nodeData) {
   u.enter()
     .append('circle')
     .attr('r', 5)
+    .attr('class', nodeClass)
     .merge(u)
     .attr('cx', function(d) {
       return d.x
@@ -63,6 +93,31 @@ function updateNodes(nodeData) {
     .attr('cy', function(d) {
       return d.y
     })
+    .on('mouseover', function (nodeDatum, _i) {
+      d3.select(this).attr("r", 7)
+      showTooltip(nodeDatum)
+    })
+    .on('mouseout', function (_nodeDatum, _i) {
+      d3.select(this).attr("r", 5)
+      hideTooltip()
+    })
 
   u.exit().remove()
+}
+
+function nodeClass(data) {
+  const id = data.id
+  if (id.includes('_web')) {
+    if (id.includes('_view.ex')) {
+      return 'node-type-view'
+    } else if (id.includes('_controller.ex')) {
+      return 'node-type-controller'
+    } else if (id.includes('/live/')) {
+      return 'node-type-live'
+    } else {
+      return ''
+    }
+  } else {
+    return ''
+  }
 }
--- lib/gviz_web/controllers/graph_controller.ex
+++ lib/gviz_web/controllers/graph_controller.ex
@@ -19,21 +19,30 @@ defmodule GVizWeb.GraphController do
 
     rows =
       graph.children
-      |> Enum.map(fn
+      |> Enum.flat_map(fn
         %Dotx.Node{id: id} ->
-          ["node", id, nil, nil, nil]
+          [["node", id, nil, nil, nil]]
 
-        %Dotx.Edge{from: from, to: to} ->
-          ["edge", nil, from.id, to.id, "compile"]
+        %Dotx.Edge{from: from, to: to} = edge ->
+          [
+            ["edge", nil, from.id, to.id, label(edge)],
+            ["node", from.id, nil, nil, nil],
+            ["node", to.id, nil, nil, nil]
+          ]
 
         element ->
           Logger.debug("ignoring node #{inspect(element)}")
+          []
       end)
+      |> Enum.uniq()
 
     csv = CSV.dump_to_iodata([headers | rows])
 
     text(conn, csv)
   end
+
+  defp label(%Dotx.Edge{attrs: %{"label" => label}}), do: label
+  defp label(%Dotx.Edge{}), do: ''
 
   def dot(conn, _params) do
     sample_dot_path = Path.join(:code.priv_dir(:gviz), "sample_xref_graph.dot")
