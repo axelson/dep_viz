72a0f97a563deea417f010a4626170c5810ea7bb
Jason Axelson <jason.axelson@gmail.com>
2021-01-26 22:27:37 -1000

    fix for running locally

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # with webpack to recompile .js and .css sources.
 config :gviz, GVizWeb.Endpoint,
   url: [host: "gviz.home.lan"],
-  https: [port: 4040],
+  http: [port: 4040],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
