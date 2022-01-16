3b620d27cc5d3816ba5b32a507151ea3d9e8faa2
Jason Axelson <jason.axelson@gmail.com>
2022-01-16 18:27:29 +0000

    Update deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule GViz.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 0.5"},
+      {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
