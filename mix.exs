4ba54b66d255dcb42065ebdbb643165e1dbc2c65
Jason Axelson <jason.axelson@gmail.com>
2022-01-16 18:31:50 +0000

    Update phoenix_live_view and live dashboard

--- mix.exs
+++ mix.exs
@@ -34,11 +34,11 @@ defmodule GViz.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6.0"},
-      {:phoenix_live_view, "~> 0.16.4"},
+      {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.5"},
+      {:phoenix_live_dashboard, "~> 0.6"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.11"},
